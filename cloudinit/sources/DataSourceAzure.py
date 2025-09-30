4b673c2c16359ed46b3b9b562367d9f827cad184
Chris Patterson <cpatterson@microsoft.com>
2025-09-30 17:28:51 +0000

    HACK: FORCE DEPLOYMENT FAILURE WITH OSPTO

--- cloudinit/sources/DataSourceAzure.py
+++ cloudinit/sources/DataSourceAzure.py
@@ -1457,7 +1457,22 @@ class DataSourceAzure(sources.DataSource):
         :returns: List of SSH keys, if requested.
         """
         report_dmesg_to_kvp()
-        kvp.report_success_to_host(vm_id=self._vm_id)
+        # HACK: FORCE ALL DEPLOYMENT FAILURE (OSPTO)
+        self._report_failure(
+            errors.ReportableError(
+                reason="forced deployment failure for testing purposes after OSPTO",
+                supporting_data=dict(
+                    details="ignore me, i'm a failure",
+                    details2="but... my mom says i'm a winner",
+                    csvcheck="this,is'fine|toparse~!@#$%^&*()[]\\{}|;':\",./<>?x\nnew\r\nline",
+                ),
+            ),
+            host_only=True,
+        )
+
+        return []
+
+        kvp.report_success_to_host()
 
         try:
             data = get_metadata_from_fabric(
